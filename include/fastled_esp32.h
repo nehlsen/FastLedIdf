63fa77e71499fe9c6880151d69c7eda5e9cea000
Nils Weigel <nils.weigel@gmail.com>
2020-04-22 08:37:32 +0200

    merged some bugfixes from FastLed (especially ESP32/RMT related)

--- include/fastled_esp32.h
+++ include/fastled_esp32.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #include "fastpin_esp32.h"
-#include "clockless_esp32.h"
+//#include "clockless_esp32.h"
 // #include "clockless_block_esp32.h"
+
+#include "clockless_rmt_esp32.h"
+//#include "clockless_i2s_esp32.h"
